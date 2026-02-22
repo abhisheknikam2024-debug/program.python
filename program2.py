ef9e52dacb086d8f866754b996b63ce58c48f925
abhisheknikam2024-debug <abhisheknikam2024@gmail.com>
2026-02-22 11:22:32 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -6,6 +6,6 @@ Name = input("entre your frist name :")
 Sirname = input("entre yuor last name:")
 print(Name)
 print(Sirname)
-fullnamw = Name + ' ' + Sirnsme
+fullname = Name + ' ' + Sirname
 
-print('hello ,',fullname , '!, welcom to python program.john')
+print('hello ,',fullname , '!, welcom to python program.')
