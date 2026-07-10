2f3fc474d52d9df1e41de20958e518ddd38ceb27
abhisheknikam2024-debug <abhisheknikam2024@gmail.com>
2026-07-10 12:33:55 +0530

    Add files via upload

--- as_4_T_2.py
+++ as_4_T_2.py
@@ -1,8 +1,20 @@
-fh = open("output.txt","wt")
-fh.write("hello, python!.")
-fh.close()
+initial_text = input("enter text to write in the file:")
 
+with open("output.txt","wt") as fh:
+    fh.write(initial_text + "\n")
+print("Data successfully writen to output.txt.\n")
 
-fh = open("output.txt","at")
-fh.write("\nLearning file handling in python \n")
-fh.close()
+#appends for adding data in the file
+addnewdata_text =input("enter additional data to add:")
+
+with open("output.txt","at") as fh:
+    fh.write(addnewdata_text + "\n")
+print("data successfully add into output.txt \n")
+
+# read and display finalcontent of fh
+
+print("final cantent of output.txt:")
+with open("output.txt","rt") as fh:
+    cantent = fh.read()
+    print(cantent)
+         
