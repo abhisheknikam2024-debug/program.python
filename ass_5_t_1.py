37d6d9da45b31d19fe02e42ff43358942c9a0946
abhisheknikam2024-debug <abhisheknikam2024@gmail.com>
2026-07-15 17:06:19 +0530

    Add files via upload

--- ass_5_t_1.py
+++ ass_5_t_1.py
@@ -10,7 +10,6 @@ std_mark = {
 user_input = input("enter a student's name:").strip()
 #strip() used tp clean a unwanted spaces by user 
 
-# regular expression
 student_name = user_input.title()     # title() use to mark a frist letter in upper case and another in lower case  
 
 if student_name in std_mark:
