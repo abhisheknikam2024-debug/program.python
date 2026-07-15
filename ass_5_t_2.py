98664d738dc035dbde3a4a108bec481464494fd5
abhisheknikam2024-debug <abhisheknikam2024@gmail.com>
2026-07-15 17:07:02 +0530

    Add files via upload

--- ass_5_t_2.py
+++ ass_5_t_2.py
@@ -1,19 +1,11 @@
 import re
 # create a list
-original_list = list(range (1, 11))  
+original_list = [1,2,3,4,5,6,7,8,9,10]
+          
+extr_str = original_list[ :5] # matchh str and covert into back list of integer
 
-# covert list to  string
-list_str = str(original_list)
+rev_list = extr_str[::-1] #reverse the extracted 
 
-#use regular expression
-pat = r"\[(\d+,\s\d+,\s\d+,\s\d+,\s\d+)" # pat for find frist five number
-match = re.search(pat, list_str)
- 
-if match:
-    extr_str = match.group(1) # matchh str and covert into back list of integer
-    extr_list = [int(x) for x in extr_str.split(", ")]
-
-    rev_list = extr_list[::-1] #reverse the extracted element
 print(f" original list: {original_list}")
-print(f"extracted frist five element: {extr_list}")
+print(f"extracted frist five element: {extr_str}")
 print(f"reversal extracted element : {rev_list}")
