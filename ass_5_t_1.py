2e080c04a7020b2d5ca15b07eceb8a8fc08b9828
abhisheknikam2024-debug <abhisheknikam2024@gmail.com>
2026-07-15 17:05:30 +0530

    Add files via upload

--- ass_5_t_1.py
+++ ass_5_t_1.py
@@ -12,11 +12,8 @@ user_input = input("enter a student's name:").strip()
 
 # regular expression
 student_name = user_input.title()     # title() use to mark a frist letter in upper case and another in lower case  
-pat = r"^[A-Za-z]+$"
-if not re.match(pat ,  user_input):
-    print("Invalid input!enter a valide name:")
+
+if student_name in std_mark:
+    print(f"{student_name}'s mark:{std_mark[student_name]}")
 else:
-    if student_name in std_mark :
-        print(F"{student_name}'s mark:{std_mark[student_name]}")
-    else:
-        print("student not found.")
+    print("student note found!")      
